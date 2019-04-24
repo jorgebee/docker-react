d8f4673cac7cc3ee8642ff3c6775a99ccb390e38
Jorge Dominguez <jorge@MacBook-Pro-de-Jorge.local>
2019-04-24 11:47:56 -0500

    Add a change from feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola, ¿Qué hace?. Perro
+          Hola, ¿Qué hace?. Cambiando desde otra Rama y haciendo el Merge.
         </p>
         <a
           className="App-link"
